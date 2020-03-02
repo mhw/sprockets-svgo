e891dd3c1f691252c432a4072b02be40561f25d8
Mark H. Wilkinson <mhw@dangerous-techniques.com>
2020-03-02 12:04:39 +0000

    Update for sprockets 4 and sassc-rails
    The `sassc-ruby` script interface is different from `ruby-sass`, and at
    the current time does not seem to support keyword arguments. Rework the
    function to accept an option map instead.
    
    The method of registering custom Sass functions is also different.
    `Functions#declare` does not exist, and the custom functions must
    be defined on specific modules provided by the processor library.
    This is complicated by there being two sassc processors in general
    use. `sprockets` provides `Sprockets::SasscProcessor` while
    `sassc-rails` provides `SassC::Rails::SassTemplate` overriding the
    processor that `sprockets` provides. Each of these processors has its
    own module on which custom functions are defined, so we extract
    the `svgo` function in to a module and include it into each.

--- lib/sprockets/svgo/sass_functions.rb
+++ lib/sprockets/svgo/sass_functions.rb
@@ -1,11 +1,20 @@
 module Sprockets
-  module SassProcessor::Functions
+  module SVGO::SassFunctions
     def svgo_data_uri(path, options = {})
-      options = options.transform_values(&:value).symbolize_keys
+      options = options.to_h.map { |k, v| [k.value.to_sym, v.value] }.to_h
+      if options[:precision]
+        options[:precision] = options[:precision].to_i
+      end
       url = sprockets_context.svgo_asset_data_uri(path.value, options)
-      Autoload::Sass::Script::String.new("url(" + url + ")")
+      Autoload::SassC::Script::Value::String.new("url(" + url + ")")
     end
-
-    Autoload::Sass::Script::Functions.declare :svgo_data_uri, [:path], var_kwargs: true
   end
 end
+
+if defined?(SassC::Rails)
+  # This module is used by sassc-rails, if loaded
+  SassC::Rails::SassTemplate::Functions.send :include, Sprockets::SVGO::SassFunctions
+end
+
+# This module is used by sprockets itself, if sassc-rails is not loaded
+Sprockets::SasscProcessor::Functions.send :include, Sprockets::SVGO::SassFunctions
